76697bc15a47f1f25fd1aad374aa9673c8e79005
jtclayt <jt.clayton92@yahoo.com>
2021-09-22 11:29:05 -0700

    Update how API url is set

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000';
+export const API_URL = process.env.REACT_APP_API_URL;
 export const INCOME_BASE_URL = `${API_URL}/budget/incomes/`;
 export const EXPENSE_BASE_URL = `${API_URL}/budget/expenses/`;
 export const USER_BASE_URL = `${API_URL}/user/`;
